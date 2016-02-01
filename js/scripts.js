4c2de26958baf296b007801f5e2a34a44e8577af
Jessica Fix <student@epicodus.com>
2016-02-01 10:30:10 -0800

    Add functionality for Jobber object

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//Business Logic
+
+function Jobber(firstName, lastName, age, city, state, expertise) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.city = city;
+  this.state = state;
+  this.expertise = expertise;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,9 @@
-describe('newJobber', function(){
-  it('will create a newJobber account with the following properties', function(){
-    var testJobber = new Jobber("Molly");
+describe('Jobber', function(){
+  it('will create a new Jobber account with the following properties', function(){
+    var testJobber = new Jobber("Molly", "Weasley", 53, "Portland", "OR", "Knitting");
     expect(testJobber.firstName).to.equal("Molly");
     expect(testJobber.lastName).to.equal("Weasley");
-    expect(testJobber.age).to.equal(28);
+    expect(testJobber.age).to.equal(53);
     expect(testJobber.city).to.equal("Portland");
     expect(testJobber.state).to.equal("OR");
     expect(testJobber.expertise).to.equal("Knitting");
