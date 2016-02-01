6571823b5b488726bc3ef67206b8da7e1e6861ec
Jessica Fix <student@epicodus.com>
2016-02-01 11:52:13 -0800

    Add jobInterests property to script

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 //Business Logic
 
-function Jobber(firstName, lastName, age, city, state, expertise) {
+function Jobber(firstName, lastName, age, city, state, jobInterests, expertise) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.age = age;
   this.city = city;
   this.state = state;
+  this.jobInterests = jobInterests;
   this.expertise = expertise;
 }
