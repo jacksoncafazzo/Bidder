df9ee82b6fbdeb0f23c6d1a3d33fd836decd7a42
Jessica Fix <student@epicodus.com>
2016-02-01 11:51:47 -0800

    Add jobInterests property to spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,12 @@
 describe('Jobber', function(){
   it('will create a new Jobber account with the following properties', function(){
-    var testJobber = new Jobber("Molly", "Weasley", 53, "Portland", "OR", "Knitting");
+    var testJobber = new Jobber("Molly", "Weasley", 53, "Portland", "OR", "Gardening", "Knitting");
     expect(testJobber.firstName).to.equal("Molly");
     expect(testJobber.lastName).to.equal("Weasley");
     expect(testJobber.age).to.equal(53);
     expect(testJobber.city).to.equal("Portland");
     expect(testJobber.state).to.equal("OR");
+    expect(testJobber.jobInterests).to.equal("Gardening")
     expect(testJobber.expertise).to.equal("Knitting");
     });
 });
