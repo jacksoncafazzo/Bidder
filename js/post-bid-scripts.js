b7eab93e0a4ac8c59ea29e96267ec31ff25009bb
Taylor Pokoj <student@epicodus.com>
2016-02-02 09:39:24 -0800

    Combined scripts from map-scripts to add map to post-a-bid page

--- js/post-bid-scripts.js
+++ js/post-bid-scripts.js
@@ -1,4 +1,41 @@
-/* Business Logic for Post a Bid */
+/* Business Logic for map */
+
+var map;
+var marker;
+var markers = [];
+var coordinates = [];
+var myLatLng;
+var imageData;
+
+function initialize () {
+  coordinates = [];
+  var myLatLong = new google.maps.LatLng(45.520705, -122.677397);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: myLatLong,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  });
+
+  map.addListener("click", function (event) {
+    var latitude = event.latLng.lat();
+    var longitude = event.latLng.lng();
+    myLatLng = {lat: latitude, lng: longitude};
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      // icon: bidder,
+      map: map,
+    });
+    markers.push(marker);
+    coordinates.push(marker.position.lat(), marker.position.lng());
+    console.log("coordinates are: " + coordinates[0] + ", " + coordinates[1]);
+    console.log("marker latlong is: " + marker.position.lat(), marker.position.lng());
+    console.log("number of marker is: " + markers.length);
+  });
+
+}
+
+/* business logic for form and bidSummary */
 
 function BidPost(jobTitle, payment, jobDuration, dateCompleted, cityState, neighborhood, bidderName) {
   this.jobTitle = jobTitle;
@@ -20,6 +57,8 @@ $(document).on('click', '#showModal', function() {
 
 
 $(document).ready(function() {
+  initialize();
+
   $("form#postBid").submit(function(event) {
     var inputtedjobTitle = $("input#jobTitle").val();
     var inputtedPayment = $("input#payment").val();
