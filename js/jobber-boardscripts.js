86369eb895dbf2010fc9b4924c777050e2726e36
Jessica <jcfix.pdx@gmail.com>
2016-02-04 14:14:47 -0800

    Add updates

---
+++ js/jobber-boardscripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".deleteButton").click(function() {
+    $("#deleteBidModal").modal('show');
+
+    var panel = $(this).parents(".panel-group");
+
+    $("#deleteBidButton").off();
+    $("#deleteBidButton").click({panel: panel}, function(event) {
+      var password = $("input#password").val();
+      if (password === "delete") {
+        event.data.panel.hide();
+      }
+      else {
+        alert("Sorry, that's not the right password.")
+      }
+
+      $("input#password").val("");
+    });
+  });
+});
