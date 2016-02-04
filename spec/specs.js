e790bbbf316968e49001fd6bae5f60b68116f898
Taylor Pokoj <student@epicodus.com>
2016-02-04 15:27:52 -0800

    Moved all specs to one specs.js page, linked all scripts that had specs. One spec is failing, not sure why.

--- spec/specs.js
+++ spec/specs.js
@@ -2,24 +2,25 @@
 /* Post a Bid Specs
 =========================*/
 
-describe('bidPost', function() {
-  it('will create a new bid summary based on the following properties', function() {
-    var testBidPost = new BidPost("Plant a salsa garden", "$100", "here is a description" "3 hours", "March 1, 2016", "Portland, OR", "Southeast Portland", "Taylor");
+describe('BidPost', function() {
+  it('will create a new bid post based on the following properties', function() {
+    var testBidPost = new BidPost("Plant a salsa garden", "$100", "3 hours", "March 1, 2016", "Portland, OR", "Southeast Portland", "Taylor", "here is a description");
     expect(testBidPost.jobTitle).to.equal("Plant a salsa garden");
     expect(testBidPost.payment).to.equal("$100");
-    expect(testBidPost.jobDescription).to.equal("here is a description");
     expect(testBidPost.jobDuration).to.equal("3 hours");
     expect(testBidPost.dateCompleted).to.equal("March 1, 2016");
     expect(testBidPost.cityState).to.equal("Portland, OR");
     expect(testBidPost.neighborhood).to.equal("Southeast Portland");
     expect(testBidPost.bidderName).to.equal("Taylor");
+    expect(testBidPost.jobDescription).to.equal("here is a description");
   });
 
-  it('will add the bidSummary method to a bidPost', function() {
-    var testBidPost = new BidPost("Plant a salsa garden", "$100", "3 hours", "March 1, 2016", "Portland, OR", "Southeast Portland", "Taylor");
+  it('will add the bidSummary method to a BidPost', function() {
+    var testBidPost = new BidPost("Plant a salsa garden", "$100", "3 hours", "March 1, 2016", "Portland, OR", "Southeast Portland", "Taylor", "here is a description");
     expect(testBidPost.bidSummary()).to.equal("Plant a salsa garden for $100")
   });
 });
+
 describe("BidSuggester", function() {
   it("will create a bid suggestion based on the following properties", function(){
     var testBidSuggester = new BidSuggester("Change bike tires", "Level 2", "$20-40");
@@ -31,5 +32,16 @@ describe("BidSuggester", function() {
   it("will output a price based on the jobs level of difficulty", function(){
     var testBidSuggester = new BidSuggester("Plant salsa garden", "Level 5", "$80-100");
     expect(testBidSuggester.range()).to.equal("$80-100");
+  });
+});
+
+describe('Jobber', function(){
+  it('will create a new Jobber account with the following properties', function(){
+    var testJobber = new Jobber("Molly", "Weasley", "97214", "Gardening", "Knitting");
+    expect(testJobber.firstName).to.equal("Molly");
+    expect(testJobber.lastName).to.equal("Weasley");
+    expect(testJobber.zipcode).to.equal("97214");
+    expect(testJobber.jobInterests).to.equal("Gardening")
+    expect(testJobber.expertise).to.equal("Knitting");
   });
 });
